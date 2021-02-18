375e7b0e34e66d3d7b7d2324850c69781666682d
rariveros <rariveros@uc.cl>
2021-02-17 23:23:21 -0300

    Actualización de detección de contornos

--- Experimento/main_exp.py
+++ Experimento/main_exp.py
@@ -6,9 +6,9 @@ from Source.deteccion import *
 from Source.procesos import *
 
 if __name__ == '__main__':
-    #recs = ROI_select(reference_file)
-    #deteccion(path, file_in, file_out, recs, 4)
-
+    recs = ROI_select(reference_file)
+    deteccion(path, file_in, file_out, recs, 3)
+    IMGs = os.listdir(path + file_out)
     PHI, T, XX, YY, Z = datos_3d(IMGs, path, file_out, 'si')
     fig = plot_ZXT(XX, YY, Z)
     plt.show()
